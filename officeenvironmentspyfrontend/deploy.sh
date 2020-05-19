6e80061ab284edf70cfa252594266aac2ab44c49
cyan <samTopaz>
2020-05-19 23:32:13 +1000

    We have added a extra commands to our deploy script for the backend, we are not really sure if they are working but we think they do.

--- officeenvironmentspyfrontend/deploy.sh
+++ officeenvironmentspyfrontend/deploy.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
 yes | gcloud app deploy app.yaml index.yaml
+scp -i ~/UNI/sem6/cloudComputing/assignments/a2/officeEnvironmentSpyBackend___Keypair.pem BackEndServer.java ubuntu@ec2-52-90-192-10.compute-1.amazonaws.com:BackEndServer.java
+ssh -i ~/UNI/sem6/cloudComputing/assignments/a2/officeEnvironmentSpyBackend___Keypair.pem ubuntu@ec2-52-90-192-10.compute-1.amazonaws.com <<"ENDSSH"
+pkill java
+java BackEndServer.java &
+echo "cats"
+ENDSSH
+
+
