7e0ec1a2121a24da0d5b7680757df01ed747444f
cyan <samTopaz>
2020-05-19 21:23:14 +1000

    We have changed the map so that it is set to Satellite by default and we have added a marker.

--- officeenvironmentspyfrontend/gMaps.js
+++ officeenvironmentspyfrontend/gMaps.js
@@ -1,8 +1,39 @@
 
 var map;
-function initMap() {
+function initMap()
+{
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        zoom: 8,
+	center: {lat: -34.397, lng: 150.644},
+	mapTypeId: 'satellite'
     });
+    map.setTilt(70);
+
+    setMarkers();
 }
+
+
+// Add marker markerLocation to map
+function setMarker(markerLocation) {
+    marker = new google.maps.Marker({
+	    position: markerLocation,
+	    map: map
+	});
+}
+
+
+// Add markers to map
+function setMarkers() {
+    local1 = new google.maps.LatLng(-34.397, 150.644);
+    setMarker(local1);
+}
+
+//var latLng = new google.maps.LatLng(lat: -34.397, lng: 150.644);
+
+// var marker = new google.maps.Marker({
+//     //    position: latLng,
+//     position: {lat: -34.397, lng: 150.644},
+//     title: "Office 1"
+// });
+
+// marker.setMap(map);
